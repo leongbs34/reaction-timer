cfafba0113ca07087f959d850f68ad257294e30a
Leong <stapler_1@hotmail.com>
2021-07-29 14:22:37 +0800

    Completed

---
+++ src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+    <h2>{{ rank }}</h2>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Ninja Fingers'
+        } else if(this.score < 400){
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    },
+}
+</script>
